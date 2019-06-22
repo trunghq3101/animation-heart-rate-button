2772b169efd8f8670efb7eb527942c166a532ee8
Trung P <trunghq3101@gmail.com>
2019-06-22 15:42:29 +0700

    init

--- app/src/main/java/com/trunghoang/generalapp/MainActivity.kt
+++ app/src/main/java/com/trunghoang/generalapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.trunghoang.generalapp
 
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageRecord.setOnClickListener(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onClick(v: View?) {
+        val animateRecord = (v as ImageView).drawable as AnimatedVectorDrawable
+        animateRecord.start()
     }
 }
